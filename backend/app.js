7a7c339c9ff1c649e8ff2c54d2b351eb30729fd3
Meenu638 <ranimeenu940@gmail.com>
2026-07-09 10:54:53 +0530

    Fix CORS for Vercel frontend

--- backend/app.js
+++ backend/app.js
@@ -14,10 +14,11 @@ const app = express();
 app.use(helmet());
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || 'https://frontend-2xd3pduiu-meenu638s-projects.vercel.app/',
+    origin: process.env.CLIENT_URL,
     credentials: true,
   })
 );
+
 app.use(compression());
 app.use(express.json({ limit: '2mb' }));
 app.use(express.urlencoded({ extended: true }));
